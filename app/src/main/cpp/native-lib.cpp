64b431da2953cff863b8ca14a26b5aa908bbd223
preqel <358795715@qq.com>
2017-08-13 11:57:49 +0800

    first commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,12 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_service_example_com_testcmake_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    int a = 1;
+    return env->NewStringUTF(hello.c_str());
+}
